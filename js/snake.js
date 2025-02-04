b8b2e9dc46a32c593af72ebe5b992c0bae33e93f
ralf <chrystianralfc@gmail.com>
2025-02-04 11:31:15 -0400

    add functions moving of snake

--- js/snake.js
+++ js/snake.js
@@ -1,27 +1,37 @@
 class Snake {
-    constructor(x, y, gridSize, ctx) {
-        this.speed = 20;
-        this.x = x;
-        this.y = y;
-        this.gridSize = gridSize;
-        this.keys = {};
+    constructor(width, height) {
+        this.x = width / 2;
+        this.y = height / 2;
+        this.gridSize = 20;
+        this.width = width;
+        this.height = height;
         this.direction = 'right';
-        this.ctx = ctx;
     }
 
-    handleKeyDown(e) {this.keys[e.key] = true;}
-
-    handleKeyUp(e) {delete this.keys[e.key];}
-    
-    moving() {
-        if (this.keys["ArrowUp"]) this.y -= this.speed;
-        if (this.keys["ArrowDown"]) this.y += this.speed;
-        if (this.keys["ArrowLeft"]) this.x -= this.speed;
-        if (this.keys["ArrowRight"]) this.x += this.speed;
+    handleKeyDown(e) {
+        const keyMap = {
+            "ArrowUp": "up",
+            "ArrowDown": "down",
+            "ArrowLeft": "left",
+            "ArrowRight": "right"
+        };
+        if (keyMap[e.key]) this.direction = keyMap[e.key];
     }
 
-    drawSnake() {
-        this.ctx.fillStyle = 'green';
-        this.ctx.fillRect(this.x, this.y, this.gridSize, this.gridSize);
+    move() {
+        switch (this.direction) {
+            case 'up': this.y -= this.gridSize; break;
+            case 'down': this.y += this.gridSize; break;
+            case 'left': this.x -= this.gridSize; break;
+            case 'right': this.x += this.gridSize; break;
+        }
+
+        this.x = Math.max(0, Math.min(this.width - this.gridSize, this.x));
+        this.y = Math.max(0, Math.min(this.height - this.gridSize, this.y));
+    }
+
+    drawSnake(ctx) {
+        ctx.fillStyle = 'green';
+        ctx.fillRect(this.x, this.y, this.gridSize, this.gridSize);
     }
 }
