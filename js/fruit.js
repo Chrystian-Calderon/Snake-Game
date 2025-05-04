06163c23b326db44dffc690af0ab435a5ddf9a84
ralf <chrystianralfc@gmail.com>
2025-05-04 13:55:55 -0400

    add sprite in the fruit

--- js/fruit.js
+++ js/fruit.js
@@ -1,19 +1,40 @@
 export class Fruit {
-    constructor(width, height) {
+    constructor(width, height, ctx) {
         this.width = width;
         this.height = height;
         this.gridSize = 20;
         this.x = Math.floor(Math.random() * width / this.gridSize) * this.gridSize;
         this.y = Math.floor(Math.random() * height / this.gridSize) * this.gridSize;
+        this.image = new Image();
+        this.image.src = '../sprites/apple.png';
+        this.imageLoaded = false;
+        this.image.onload = () => {
+            this.imageLoaded = true;
+        }
     }
 
-    randomPosition() {
-        this.x = Math.floor(Math.random() * this.width / this.gridSize) * this.gridSize;
-        this.y = Math.floor(Math.random() * this.height / this.gridSize) * this.gridSize;
+    randomPosition(snakeBody) {
+        let isRepeatRandomPosition = false;
+        let x, y;
+        // console.log('1', this.x, this.y);
+        do {
+            x = Math.floor(Math.random() * this.width / this.gridSize) * this.gridSize;
+            y = Math.floor(Math.random() * this.height / this.gridSize) * this.gridSize;
+            // console.table(JSON.parse(JSON.stringify(snakeBody)));
+            // console.log('2', x, y);
+            
+            isRepeatRandomPosition = snakeBody.some(part => this.x === part.x && this.y === part.y);
+        } while (isRepeatRandomPosition);
+        // console.log('end');
+        this.x = x;
+        this.y = y;
     }
 
     drawFruit(ctx) {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.gridSize, this.gridSize);
+        if (this.imageLoaded) {
+            ctx.drawImage(this.image, this.x, this.y, this.gridSize, this.gridSize)
+        }
+        // ctx.fillStyle = 'red';
+        // ctx.fillRect(this.x, this.y, this.gridSize, this.gridSize);
     }
 }
